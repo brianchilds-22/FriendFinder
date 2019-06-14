30f36f8f79372cff9c8452c08887deeed080a692
brianchilds-22 <brianchilds22@msn.com>
2019-06-13 21:29:24 -0600

    working app

---
+++ app/data/friends.js
@@ -0,0 +1,70 @@
+var friends = [
+
+{
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+    "name":"Mark",
+    "photo":"https://cdn.pixabay.com/photo/2016/03/09/15/10/man-1246508__340.jpg",
+    "scores":[
+        5,
+        3,
+        2,
+        4,
+        4,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]   
+  },
+  {
+    "name":"Jim",
+    "photo":"https://amp.businessinsider.com/images/5899ffcf6e09a897008b5c04-750-750.jpg",
+    "scores":[
+        3,
+        3,
+        2,
+        4,
+        5,
+        5,
+        1,
+        2,
+        3,
+        3
+    ]
+  },
+  {
+    "name":"Lori",
+    "photo":"https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg",
+    "scores":[
+        3,
+        3,
+        2,
+        4,
+        5,
+        5,
+        1,
+        2,
+        3,
+        3
+    ]
+  },
+];
+
+module.exports = friends;
+  
